ffe43b98c83d563640e8aee6e5aa4633b134a23a
SATHWIKA2002 <98942206+SATHWIKA2002@users.noreply.github.com>
2024-05-20 22:41:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MainPage from './shoppingfolder/pages/MainPage'; // Corrected path
+
+const App = () => {
+  return (
+    <div>
+ {/* Corrected typo */}
+      <MainPage />
+    </div>
+  );
+};
+
+export default App;
+
